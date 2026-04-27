fb44c916f60c24ecd8a5392d3d1f4573e2330ba3
VanessaEscutia <vanessaescutia19@gmail.com>
2026-04-27 15:05:39 -0600

    exc readme

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-
-from .CategoriaModel import Categoria
-from .CursoMode import Curso
-from .AlumnoModel import Alumno
-from .EnrollmentModel import enrollments
-
-__all__ = ['db', 'Categoria', 'Curso', 'Alumno', 'enrollments']
